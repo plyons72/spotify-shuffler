8a03c7da62e79fb0680733394f957d6724b39270
Patrick Lyons <plyons72@gmail.com>
2024-03-03 21:31:38 -0500

    Workaround for the 100 song limit through API calls

--- spotify-shuffler.py
+++ spotify-shuffler.py
@@ -26,30 +26,60 @@ sp = spotipy.Spotify(
 
 # Function to get all songs from the playlist
 def get_playlist_songs(playlist_id):
-    print("Getting playlist songs")
-    results = sp.playlist_items(playlist_id)
-    songs = [item['track']['id'] for item in results['items']]
-    return songs
+    # Get total number of tracks so we can loop over all of them. API only allows 100 at a time
+    total_tracks = sp.playlist_items(playlist_id, fields='total')['total']
+    songs = []
+    index = 0
+    while index < total_tracks-1:
+        results = (sp.playlist_items(playlist_id, fields='items.track.id', offset=index))
+        for item in results['items']:
+            songs.append(item['track']['id'])
+        index += 100
+    return songs,total_tracks
 
 # Function to delete all songs from the playlist
-def delete_playlist_songs(playlist_id, songs):
-    sp.playlist_remove_all_occurrences_of_items(playlist_id, songs)
+def delete_playlist_songs(playlist_id, songs, total_tracks):
+    first = 0
+    last = 99
+    # If we have more than 100 songs, loop through until we've deleted all songs in chunks of 100
+    if len(songs) > 100:
+        while first < total_tracks - 1:
+            sp.playlist_remove_all_occurrences_of_items(playlist_id, songs[first:last])
+            first = first + 100
+            last = last + 100 if last + 100 < total_tracks - 1 else total_tracks - 1
+    else:
+        sp.playlist_remove_all_occurrences_of_items(playlist_id, songs)
 
 # Function to add songs to the playlist in a shuffled order
-def add_songs_shuffled(playlist_id, song_ids):
-    random.shuffle(song_ids)
-    sp.playlist_add_items(playlist_id, song_ids)
+def add_songs_shuffled(playlist_id, songs, total_tracks):
+    # Helpers to keep track of our slices
+    first = 0
+    last = 99
+    # Randomize the list
+    random.shuffle(songs)
+    # If we have more than 100 songs, loop through until we've added all songs in chunks of 100
+    if len(songs) > 100:
+        while first < total_tracks - 1:
+            sp.playlist_add_items(playlist_id, songs[first:last])
+            first = first + 100
+            last = last + 100 if last + 100 < total_tracks - 1 else total_tracks - 1
+    else:
+        sp.playlist_add_items(playlist_id, songs)
 
-# Get all songs from the playlist
-print("Getting list of songs from playlist")
-playlist_songs = get_playlist_songs(PLAYLIST_ID)
+def main():
+    # Get all songs from the playlist
+    print("Getting list of songs from playlist")
+    playlist_songs,total_tracks = get_playlist_songs(PLAYLIST_ID)
 
-# Delete all songs from the playlist
-print("Deleting songs from playlist")
-delete_playlist_songs(PLAYLIST_ID, playlist_songs)
+    # Delete all songs from the playlist
+    print("Deleting songs from playlist")
+    delete_playlist_songs(PLAYLIST_ID, playlist_songs, total_tracks)
 
-# Add the songs back to the playlist in a shuffled order
-print("Shuffling and adding songs back in")
-add_songs_shuffled(PLAYLIST_ID, playlist_songs)
+    # Add the songs back to the playlist in a shuffled order
+    print("Shuffling and adding songs back in")
+    add_songs_shuffled(PLAYLIST_ID, playlist_songs, total_tracks)
 
-print("Songs shuffled and added back to the playlist successfully.")
+    print("Songs shuffled and added back to the playlist successfully.")
+
+if __name__ == "__main__":
+    main()
