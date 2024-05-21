3b5b2e2acb4af62c6dca35cb882c83d1be0a599a
Patrick Lyons <plyons72@gmail.com>
2024-05-20 21:58:23 -0400

    Update readme, make headless, add logs, and add ability to shuffle multiple playlists

--- README.md
+++ README.md
@@ -1,34 +1,51 @@
 # spotify-shuffler
-Get all entries from spotify playlist, delete them, shuffle them, then add them back in, since 
+I make no claims as to the safety of this program. I advise anyone who chooses to use it to 
+create a copy of their playlist and run the program in that copy. In the event that the program 
+crashes mid-run, songs may have been deleted and not added back in. The original playlist can 
+then backfill the missing songs to the copy. This is something I intend to work on in the future
+
+Get all entries from spotify playlist(s), delete them, shuffle them, then add them back in, since 
 Spotify can't figure out a simple randomizer function. 
 
 Create a spotify app at https://developer.spotify.com/ to generate some API credentials. Set 
-client ID and secret appropriately in script. The Redirect URI doesnt matter. Set it to localhost 
-if you want, but make sure you set it in the spotify developer site when creating the App as well... 
-The important thing to note is that when the script is run, it will open a browser, have you sign in, 
-and will redirect you to this site. If you get an error on the webpage, that's fine. Just copy the 
-address in the browser and paste it in the terminal where the script is run. 
+client ID and secret appropriately in script. The Redirect URI doesnt matter. Keep it set it to 
+localhost if you want, but make sure you set it in the spotify developer site when creating the app 
+as well. The important thing to note is that when the script is run, it will open a browser, have 
+you sign in, and will redirect you to this site. If you get an error on the webpage, that's fine. 
+Just copy the address in the browser and paste it in the terminal where the script is run. 
 
 ```
 $ python3 spotify-shuffler.py 
-Getting list of songs from playlist
-Getting playlist songs
-Enter the URL you were redirected to: http://localhost/?code=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-Deleting songs from playlist
-Shuffling and adding songs back in
-Songs shuffled and added back to the playlist successfully.
+Go to the following URL: https://accounts.spotify.com/authorize?client_id=x&response_type=code&redirect_uri=http%3A%2F%2Flocalhost&scope=playlist-modify-private&show_dialog=True
+Enter the URL you were redirected to: http://localhost/?code=x
+Shuffling songs in playlist: "My Songs"
+    Getting list of songs from playlist
+    Total tracks in playlist: 333
+    Deleting songs from playlist
+    Shuffling and adding songs back in
+    Songs shuffled and added back to the playlist successfully.
+
+Shuffling songs in playlist: "My other songs"
+    Getting list of songs from playlist
+    Total tracks in playlist: 622
+    Deleting songs from playlist
+    Shuffling and adding songs back in
+    Songs shuffled and added back to the playlist successfully.
 ```
 
-Note the `Enter the URL you were redirected to:` line. It just needs to extract a code from the 
-redirect url so that it can proceed with client auth and perform the shuffling. 
+The script will run and present you with a url to visit. Click the link, press the accept button, 
+and you will be redirected to a new page. This should look like a dead link, but in the search bar 
+you will see a URL. Copy the full url and paste it back in the terminal where the script was run. 
 
 If everything works correctly, the playlist will be shuffled, and a cached token will be saved 
-locally as token.txt so that further runs of the script do not require reauth. 
+locally as `token.txt` so that subsequent runs of the script do not require reauthorization. 
 
-The idea here is to shuffle the playlist, and then play it through, without using spotify's 
-shuffle algorithm, since once again, their developers dont know how basic randomization 
-works, and I hear the same 50 songs over and over again in a 600 song playlist. 
+The idea here is to shuffle the playlist, and then play it through without using Spotify's 
+shuffle algorithm, since their developers dont know how basic randomization works, and I 
+hear the same 50 songs over and over again in a 600 song playlist. 
 
-*Note* The script will remove duplicate songs. If you want to keep them, comment out `songs = list(set(songs))` from `get_playlist_songs()`. 
+We are limited by Spotify's Rate Limiting algorithm, and a limit of 100 songs per transaction 
+(get, post, delete), so we have to manage slices of songs to work with that does not exceed 
+either of these limits
 
 Enjoy. 
--- spotify-shuffler.py
+++ spotify-shuffler.py
@@ -8,11 +8,13 @@ CLIENT_ID = ""
 CLIENT_SECRET = ""
 REDIRECT_URI = "http://localhost"
 
-# Set your spotify playlist ID. If you open spotify, click on the playlist, 
+# Set your spotify playlist ID(s). If you open spotify, click on the playlist, 
 # and create a link to it, the ID will be where the Xs are below
 # https://open.spotify.com/playlist/XXXXXXXXXXXXXXXXXXXXXX?si=YYYYYYYYYYYYYYYYY
-PLAYLIST_ID = ""
+# Program can operate on a single playlist or multiple if list is provided
+PLAYLIST_IDS = ["", ""]
 
+# Instantiate spotipy client
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -20,70 +22,78 @@ sp = spotipy.Spotify(
         redirect_uri=REDIRECT_URI,
         scope="playlist-modify-private",
         show_dialog=True,
+        open_browser=False,
         cache_path="token.txt"
-    ))
+    )
+)
+
 
 # Function to get all songs from the playlist
 def get_playlist_songs(playlist_id):
     # Get total number of tracks so we can loop over all of them. API only allows 100 at a time
-    total_tracks = sp.playlist_items(playlist_id, fields='total')['total']
-    print("Total tracks in playlist: {}".format(total_tracks))
+    num_tracks = sp.playlist_items(playlist_id, fields='total')['total']
     songs = []
     index = 0
-    while index < total_tracks - 1:
+    while index < num_tracks - 1:
         # Call returns dict of items:track:id. Just get a list of IDs to operate on
         results = (sp.playlist_items(playlist_id, fields='items.track.id', offset=index))
         for item in results['items']:
             songs.append(item['track']['id'])
         index = index + 100
-    # Make song list a set to remove duplicates
-    print("Removing duplicate songs...")
-    songs = list(set(songs))
     return songs, len(songs)
 
 # Function to delete all songs from the playlist
-def delete_playlist_songs(playlist_id, songs, total_tracks):
+def delete_playlist_songs(playlist_id, songs, num_tracks):
+    # Helpers to keep track of our slices
     first = 0
     last = 100
     # If we have more than 100 songs, loop through until we've deleted all songs in chunks of 100
-    if total_tracks > 100:
-        while first < total_tracks:
+    if num_tracks > 100:
+        while first < num_tracks:
             sp.playlist_remove_all_occurrences_of_items(playlist_id, songs[first:last])
             first = first + 100
-            last = last + 100 if last + 100 < total_tracks else total_tracks
+            last = last + 100 if last + 100 < num_tracks else num_tracks
     else:
         sp.playlist_remove_all_occurrences_of_items(playlist_id, songs)
 
+
 # Function to add songs to the playlist in a shuffled order
-def add_songs_shuffled(playlist_id, songs, total_tracks):
+def add_songs_shuffled(playlist_id, songs, num_tracks):
     # Helpers to keep track of our slices
     first = 0
     last = 100
     # Randomize the list
     random.shuffle(songs)
     # If we have more than 100 songs, loop through until we've added all songs in chunks of 100
-    if total_tracks > 100:
-        while first < total_tracks:
+    if num_tracks > 100:
+        while first < num_tracks:
             sp.playlist_add_items(playlist_id, songs[first:last])
             first = first + 100
-            last = last + 100 if last + 100 < total_tracks else total_tracks
+            last = last + 100 if last + 100 < num_tracks else num_tracks
     else:
         sp.playlist_add_items(playlist_id, songs)
 
 def main():
-    # Get all songs from the playlist
-    print("Getting list of songs from playlist")
-    playlist_songs,total_tracks = get_playlist_songs(PLAYLIST_ID)
+    # Loop over list of playlists
+    for playlist_id in PLAYLIST_IDS:
+        name = sp.playlist(playlist_id, fields="name")
+        print("\nShuffling songs in playlist: \"{}\"".format(name['name']))
 
-    # Delete all songs from the playlist
-    print("Deleting songs from playlist")
-    delete_playlist_songs(PLAYLIST_ID, playlist_songs, total_tracks)
+        # Get all songs from the playlist
+        print("\tGetting list of songs from playlist")
+        playlist_songs, num_tracks = get_playlist_songs(playlist_id)
+        print("\tTotal tracks in playlist: {}".format(num_tracks))
 
-    # Add the songs back to the playlist in a shuffled order
-    print("Shuffling and adding songs back in")
-    add_songs_shuffled(PLAYLIST_ID, playlist_songs, total_tracks)
 
-    print("Songs shuffled and added back to the playlist successfully.")
+        # Delete all songs from the playlist
+        print("\tDeleting songs from playlist")
+        delete_playlist_songs(playlist_id, playlist_songs, num_tracks)
+
+        # Add the songs back to the playlist in a shuffled order
+        print("\tShuffling and adding songs back in")
+        add_songs_shuffled(playlist_id, playlist_songs, num_tracks)
+
+        print("\tSongs shuffled and added back to the playlist successfully.")
 
 if __name__ == "__main__":
     main()
